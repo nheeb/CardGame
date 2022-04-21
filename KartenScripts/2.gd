08c56fa5dbee0b6afd13a00d5893652f6f1135d9
Ni3lz <n62611iels@gmail.com>
2022-04-21 13:09:32 +0200

    weqwe

--- KartenScripts/2.gd
+++ KartenScripts/2.gd
@@ -1,7 +1,12 @@
 extends Karte
+const TREE = preload("res://Objects/Tree.tscn")
 
 func _init():
 	card_type = 2
 
 func play_effect():
 	print("Confederacy")
+	var new_baum = TREE.instance()
+	get_tree().current_scene.add_child(new_baum)
+	new_baum.translation = GameInfo.mouse_position
+	queue_free()
