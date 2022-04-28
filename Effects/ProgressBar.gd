91b3600ce5419ad7a8b7af4b1af857796c1052e1
nheeb <nitai64@yahoo.de>
2022-04-28 19:38:11 +0200

    ProgressBar

---
+++ Effects/ProgressBar.gd
@@ -0,0 +1,7 @@
+extends Spatial
+
+export var progress := 0.0 setget set_progress
+
+func set_progress(x):
+	progress = clamp(x, 0.0, 1.0)
+	$MeshInstance.get("material/0").set("shader_param/completeness", progress)
