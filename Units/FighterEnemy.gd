93d37c979e4095448c6e370920ca53709f8ab65d
Ni3lz <n62611iels@gmail.com>
2022-04-29 14:35:18 +0200

    kwqnlwq

--- Units/FighterEnemy.gd
+++ Units/FighterEnemy.gd
@@ -1,5 +1,18 @@
 extends Spatial
 
 
+var hp :int= 100
+var max_hp :int= 100
+
 func _ready():
 	GameInfo.enemyListe.append(self)
+	hp_change(max_hp)
+	
+func hp_change(new_hp):
+	hp = new_hp
+	if hp <= 0:
+		GameInfo.enemyListe.erase(self)
+		self.queue_free()
+	$ProgressBar.progress = hp/float(max_hp)
+
+
