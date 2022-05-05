f9ea5c4829d0d31535c1ff25606e0ab26299671a
nheeb <nitai64@yahoo.de>
2022-05-05 16:03:08 +0200

    Goblin Model

---
+++ Models/GoblinModel.gd
@@ -0,0 +1,10 @@
+extends Spatial
+
+func transition_to(animation_name : String, duration : float) -> void:
+	var target_blend
+	match(animation_name):
+		"idle":
+			target_blend = 0
+		"walk":
+			target_blend = 1
+	$TransitionTween.interpolate_property($AnimationTree, "parameters/BlendWalk/blend_amount", $AnimationTree.get("parameters/BlendWalk/blend_amount"), target_blend, duration)
