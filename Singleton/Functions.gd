3ef689b71625968753bb35ca26ffa112de6a23eb
nheeb <nitai64@yahoo.de>
2022-05-15 22:46:27 +0200

    Round Progress set color

--- Singleton/Functions.gd
+++ Singleton/Functions.gd
@@ -1,13 +1,5 @@
 extends Spatial
 
-#var bezugsobjekt : Spatial
-
-#func distance_compare(obj1, obj2):
-#	return distance_to_object(obj1) <= distance_to_object(obj2)
-
-#func distance_to_object(obj):
-#	return obj.global_transform.origin.distance_to(bezugsobjekt.global_transform.origin)	
-
 func get_nearest_object_to_target(target: Spatial, objects: Array):
 	if objects.empty(): return null
 	var dist := target.global_transform.origin.distance_to(objects[0].global_transform.origin)
